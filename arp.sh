bcae3d51f721235f6283848e8c3577ec0547af31
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-10-29 23:15:00 +0500

    1

---
+++ arp.sh
@@ -0,0 +1,4 @@
+sudo arp -i enp2s0f3 -s 192.168.99.20 00:90:0b:c3:49:89
+sudo arp -i enp2s0f3 -s 192.168.99.30 00:90:0b:c3:4e:c9
+sudo arp -i enp2s0f3 -s 192.168.99.40 00:90:0b:c3:46:71
+sudo arp -i enp2s0f3 -s 192.168.99.50 00:90:0b:c3:39:49
